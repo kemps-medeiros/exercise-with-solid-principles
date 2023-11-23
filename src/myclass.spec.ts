bd73cac9c297aa053ff867b775a9b0300e585041
Kemps Medeiros <kempsmm@gmail.com>
2023-11-23 17:07:15 -0300

    fix: remove tests file

--- src/myclass.spec.ts
+++
@@ -1,9 +0,0 @@
-import { MyClass } from "./myclass";
-
-describe('MyClass', () => {
-  it('should add two numbers', () => {
-    const myClass = new MyClass();
-    const result = myClass.add(2, 3);
-    expect(result).toBe(5);
-  });
-});
--- src/myclass.ts
+++
@@ -1,5 +0,0 @@
-export class MyClass {
-    add(a: number, b: number): number {
-      return a + b;
-    }
-  }
